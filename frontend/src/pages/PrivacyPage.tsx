75c22395abff39eb6d4e694638fbb377e72c4e8a
Max Daniels <maxadaniels@icloud.com>
2023-09-26 10:41:33 +0200

    modified:   frontend/src/pages/PrivacyPage.tsx

--- frontend/src/pages/PrivacyPage.tsx
+++ frontend/src/pages/PrivacyPage.tsx
@@ -1,9 +1,27 @@
 const PrivacyPage = () => {
-    return ( 
+    return (
         <div>
-            <p>We care about your privacy. Promise!</p>
+            <h1>Privacy Policy</h1>
+            <p>Your privacy is important to us. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website.</p>
+
+            <h2>Information We Collect</h2>
+            <p>We may collect personal information such as your name, email address, and contact information when you fill out forms or interact with our website.</p>
+
+            <h2>How We Use Your Information</h2>
+            <p>We use the information we collect to provide and improve our services, communicate with you, and personalize your experience on our website.</p>
+
+            <h2>Disclosure of Your Information</h2>
+            <p>We may share your information with trusted third parties for specific purposes, such as analytics or marketing, but we will never sell your personal information.</p>
+
+            <h2>Security</h2>
+            <p>We take the security of your information seriously and implement measures to protect it from unauthorized access or disclosure.</p>
+
+            <h2>Changes to this Privacy Policy</h2>
+            <p>We may update this Privacy Policy from time to time to reflect changes in our practices. Please review this page regularly for any updates.</p>
+
+            <p>If you have any questions or concerns about our Privacy Policy, please <a href="/contact">contact us</a>.</p>
         </div>
-     );
+    );
 }
- 
+
 export default PrivacyPage;
